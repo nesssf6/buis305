f67672c599e3867911f6b8359982f0b7faf768fb
nesta yusao kajempou <nestayusao@gmail.com>
2023-11-08 13:17:13 -0500

    new lab(3)

---
+++ Lab(4).py
@@ -0,0 +1,20 @@
+# count from 0 to 10
+for count in range(0,10):
+    print(count)
+# count from 1 to 10
+for count in range(1,11):
+    print(count)
+for count in range(1,10,2):
+    print(count,end="")
+
+π = 3.14
+Radius = float(input("enter the radius of the given circle: "))
+area_of_the_circle = π * Radius * Radius
+
+print(" The area of the given circle is: ", area_of_the_circle)
+length = float(input("Please enter the length of the rectangle:"))
+breadth = float(input("Please enter breath of the rectangle:  "))
+
+area = (length * breadth)
+print("The Area of a Rectangle using:", length, "and", breadth, " = ", area)
+print("34")
